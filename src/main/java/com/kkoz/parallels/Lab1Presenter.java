a6618d2002aa7a6c7bf22a3cc4f5e4ed085fe799
Konstantin Kozyrev <k.kozyrev@citysens.ru>
2023-10-08 23:35:35 +0700

    Код чутка лучше

--- src/main/java/com/kkoz/parallels/Lab1Presenter.java
+++ src/main/java/com/kkoz/parallels/Lab1Presenter.java
@@ -1,9 +1,87 @@
 package com.kkoz.parallels;
 
+import com.vaadin.flow.component.upload.SucceededEvent;
+import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lab1Presenter {
     private final Lab1View view;
 
+    private List<List<Integer>> redMatrix;
+    private List<List<Integer>> greenMatrix;
+    private List<List<Integer>> blueMatrix;
+
     public Lab1Presenter(Lab1View view) {
         this.view = view;
+    }
+
+    public void onPhotoUploaded(SucceededEvent event, MultiFileMemoryBuffer buffer) {
+        try {
+            var inputStream = buffer.getInputStream(event.getFileName());
+            view.refreshSourcePhotoSection(inputStream);
+            var bufferedImage = ImageIO.read(buffer.getInputStream(event.getFileName()));
+            var width = bufferedImage.getWidth();
+            var height = bufferedImage.getHeight();
+            redMatrix = new ArrayList<>(width);
+            greenMatrix = new ArrayList<>(width);
+            blueMatrix = new ArrayList<>(width);
+            for (var x = 0; x < width; x++) {
+                var redHeight = new ArrayList<Integer>(height);
+                var greenHeight = new ArrayList<Integer>(height);
+                var blueHeight = new ArrayList<Integer>(height);
+                for (var y = 0; y < height; y++) {
+                    var rgb = bufferedImage.getRGB(x, y);
+                    var color = new java.awt.Color(rgb);
+                    redHeight.add(color.getRed());
+                    greenHeight.add(color.getGreen());
+                    blueHeight.add(color.getBlue());
+                }
+                redMatrix.add(x, redHeight);
+                greenMatrix.add(x, greenHeight);
+                blueMatrix.add(x, blueHeight);
+            }
+
+            var bufferedImageRed = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            var bufferedImageGreen = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            var bufferedImageBlue = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+            for (var x = 0; x < width; x++) {
+                var redHeight = redMatrix.get(x);
+                var greenHeight = greenMatrix.get(x);
+                var blueHeight = blueMatrix.get(x);
+                for (var y = 0; y < height; y++) {
+                    var red = redHeight.get(y);
+                    var green = greenHeight.get(y);
+                    var blue = blueHeight.get(y);
+                    bufferedImageRed.setRGB(x, y, new Color(red, 0, 0).getRGB());
+                    bufferedImageGreen.setRGB(x, y, new Color(0, green, 0).getRGB());
+                    bufferedImageBlue.setRGB(x, y, new Color(0, 0, blue).getRGB());
+                }
+            }
+
+            view.refreshChannelsPhotoSection(
+                getInputStreamFromBufferedImage(bufferedImageRed),
+                getInputStreamFromBufferedImage(bufferedImageGreen),
+                getInputStreamFromBufferedImage(bufferedImageBlue)
+            );
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public InputStream getInputStreamFromBufferedImage(BufferedImage bufferedImage) throws IOException {
+        var byteBuffer = new ByteArrayOutputStream();
+        ImageIO.write(bufferedImage, "jpeg", byteBuffer);
+        return new ByteArrayInputStream(byteBuffer.toByteArray());
     }
 }
--- src/main/java/com/kkoz/parallels/Lab1View.java
+++ src/main/java/com/kkoz/parallels/Lab1View.java
@@ -1,7 +1,6 @@
 package com.kkoz.parallels;
 
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -10,25 +9,14 @@ import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.StreamResource;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 @Route("/labs/1")
 public class Lab1View extends VerticalLayout {
     private final Lab1Presenter presenter;
-    private Component sourceImageSection = new Div();
-    private Component redImageSection = new Div();
-    private Component greenImageSection = new Div();
-    private Component blueImageSection = new Div();
-    private List<List<Integer>> redMatrix;
-    private List<List<Integer>> greenMatrix;
-    private List<List<Integer>> blueMatrix;
+
+    private HorizontalLayout sourceImageSection = new HorizontalLayout();
+    private HorizontalLayout channelsImageSection = new HorizontalLayout();
 
     Lab1View() {
         presenter = new Lab1Presenter(this);
@@ -36,7 +24,7 @@ public class Lab1View extends VerticalLayout {
         add(
             createUploadPhotoSection(),
             sourceImageSection,
-            new HorizontalLayout(redImageSection, greenImageSection, blueImageSection)
+            channelsImageSection
         );
     }
 
@@ -44,94 +32,34 @@ public class Lab1View extends VerticalLayout {
         var buffer = new MultiFileMemoryBuffer();
         var upload = new Upload(buffer);
 
-        upload.addSucceededListener(e -> {
-            var inputStream = buffer.getInputStream(e.getFileName());
+        upload.addSucceededListener(event -> {
+            presenter.onPhotoUploaded(event, buffer);
             upload.clearFileList();
-            createSourcePhotoSection(inputStream);
-            try {
-                var bufferedImage = ImageIO.read(buffer.getInputStream(e.getFileName()));
-                var width = bufferedImage.getWidth();
-                var height = bufferedImage.getHeight();
-                redMatrix = new ArrayList<>(width);
-                greenMatrix = new ArrayList<>(width);
-                blueMatrix = new ArrayList<>(width);
-                for (var x = 0; x < width; x++) {
-                    var redHeight = new ArrayList<Integer>(height);
-                    var greenHeight = new ArrayList<Integer>(height);
-                    var blueHeight = new ArrayList<Integer>(height);
-                    for (var y = 0; y < height; y++) {
-                        var rgb = bufferedImage.getRGB(x, y);
-                        var color = new java.awt.Color(rgb);
-                        redHeight.add(color.getRed());
-                        greenHeight.add(color.getGreen());
-                        blueHeight.add(color.getBlue());
-                    }
-                    redMatrix.add(x, redHeight);
-                    greenMatrix.add(x, greenHeight);
-                    blueMatrix.add(x, blueHeight);
-                }
-
-                var bufferedImageRed = new java.awt.image.BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-                var bufferedImageGreen = new java.awt.image.BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-                var bufferedImageBlue = new java.awt.image.BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-                for (var x = 0; x < width; x++) {
-                    var redHeight = redMatrix.get(x);
-                    var greenHeight = greenMatrix.get(x);
-                    var blueHeight = blueMatrix.get(x);
-                    for (var y = 0; y < height; y++) {
-                        var red = redHeight.get(y);
-                        var green = greenHeight.get(y);
-                        var blue = blueHeight.get(y);
-                        bufferedImageRed.setRGB(x, y, new java.awt.Color(red, 0, 0).getRGB());
-                        bufferedImageGreen.setRGB(x, y, new java.awt.Color(0, green, 0).getRGB());
-                        bufferedImageBlue.setRGB(x, y, new java.awt.Color(0, 0, blue).getRGB());
-                    }
-                }
-
-                var redByteBuffer = new ByteArrayOutputStream();
-                ImageIO.write(bufferedImageRed, "jpeg", redByteBuffer);
-                var redInputStream = new ByteArrayInputStream(redByteBuffer.toByteArray());
-                var tmpRedImageSection = new Image(
-                    new StreamResource("Красный", () -> redInputStream),
-                    ""
-                );
-                replace(redImageSection, tmpRedImageSection);
-                redImageSection = tmpRedImageSection;
-
-                var greenByteBuffer = new ByteArrayOutputStream();
-                ImageIO.write(bufferedImageGreen, "jpeg", greenByteBuffer);
-                var greenInputStream = new ByteArrayInputStream(greenByteBuffer.toByteArray());
-                var tmpGreenImageSection = new Image(
-                    new StreamResource("Зелёный", () -> greenInputStream),
-                    ""
-                );
-                replace(greenImageSection, tmpGreenImageSection);
-                greenImageSection = tmpGreenImageSection;
-
-                var blueByteBuffer = new ByteArrayOutputStream();
-                ImageIO.write(bufferedImageBlue, "jpeg", blueByteBuffer);
-                var blueInputStream = new ByteArrayInputStream(blueByteBuffer.toByteArray());
-                var tmpBlueImageSection = new Image(
-                    new StreamResource("Синий", () -> blueInputStream),
-                    ""
-                );
-                replace(blueImageSection, tmpBlueImageSection);
-                blueImageSection = tmpBlueImageSection;
-
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
         });
 
         return upload;
     }
 
-    private void createSourcePhotoSection(InputStream imageStream) {
-        var tmpImageSection = new Image(
-            new StreamResource("Исходное фото", () -> imageStream),
+    private void addPhotoToSection(HorizontalLayout imageSection, InputStream imageStream, String name) {
+        var image = new Image(
+            new StreamResource(name, () -> imageStream),
             ""
         );
-        replace(sourceImageSection, tmpImageSection);
-        sourceImageSection = tmpImageSection;
+        image.setWidthFull();
+        imageSection.add(image);
+    }
+
+    public void refreshSourcePhotoSection(InputStream sourceImageStream) {
+        sourceImageSection.removeAll();
+        addPhotoToSection(sourceImageSection, sourceImageStream, "Исходное фото");
+    }
+
+    public void refreshChannelsPhotoSection(InputStream firstChannel,
+                                            InputStream secondChannel,
+                                            InputStream thirdChannel) {
+        channelsImageSection.removeAll();
+        addPhotoToSection(channelsImageSection, firstChannel, "Первый канал");
+        addPhotoToSection(channelsImageSection, secondChannel, "Второй канал");
+        addPhotoToSection(channelsImageSection, thirdChannel, "Третий канал");
     }
 }
