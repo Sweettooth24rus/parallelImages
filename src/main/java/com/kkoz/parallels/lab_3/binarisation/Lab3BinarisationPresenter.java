7231fea969962179261ef5654aeea520af640cfa
Konstantin Kozyrev <k.kozyrev@citysens.ru>
2024-01-29 00:38:14 +0700

    Сужение расширение

--- src/main/java/com/kkoz/parallels/lab_3/binarisation/Lab3BinarisationPresenter.java
+++ src/main/java/com/kkoz/parallels/lab_3/binarisation/Lab3BinarisationPresenter.java
@@ -11,6 +11,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class Lab3BinarisationPresenter extends Presenter<Lab3BinarisationView> {
 
@@ -146,7 +147,7 @@ public class Lab3BinarisationPresenter extends Presenter<Lab3BinarisationView> {
                 }
             }
 
-            view.refreshFilterPhotosSection(getInputStreamFromBufferedImage(bufferedImage));
+            view.refreshFilterPhotosSection(bufferedImage, getInputStreamFromBufferedImage(bufferedImage));
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -156,5 +157,426 @@ public class Lab3BinarisationPresenter extends Presenter<Lab3BinarisationView> {
         var byteBuffer = new ByteArrayOutputStream();
         ImageIO.write(bufferedImage, "jpeg", byteBuffer);
         return new ByteArrayInputStream(byteBuffer.toByteArray());
+    }
+
+    public void dilation(BufferedImage oldBufferedImage, List<List<String>> dilationCoefficientsValues) {
+        try {
+            var coefCenterX = 0;
+            var coefCenterY = 0;
+            var dilationCoefficients = new int[dilationCoefficientsValues.get(0).size()][dilationCoefficientsValues.size()];
+
+            for (var i = 0; i < dilationCoefficientsValues.get(0).size(); i++) {
+                dilationCoefficients[i] = new int[dilationCoefficientsValues.size()];
+                for (var j = 0; j < dilationCoefficientsValues.size(); j++) {
+                    dilationCoefficients[i][j] = Integer.parseInt(dilationCoefficientsValues.get(i).get(j)) * 255;
+                    if (dilationCoefficients[i][j] == 510) {
+                        coefCenterX = i;
+                        coefCenterY = j;
+                    }
+                }
+            }
+
+            var width = oldBufferedImage.getWidth();
+            var height = oldBufferedImage.getHeight();
+
+            var bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+            var oldMatrix = new int[width][height];
+            var newMatrix = new int[width][height];
+
+            var start = System.currentTimeMillis();
+
+            for (var x = 0; x < width; x++) {
+                var oldHeight = new int[height];
+                var newHeight = new int[height];
+
+                for (var y = 0; y < height; y++) {
+                    var rgb = oldBufferedImage.getRGB(x, y);
+                    var color = new Color(rgb);
+
+                    oldHeight[y] = (int) (0.2125 * color.getRed() + 0.7154 * color.getGreen() + 0.0721 * color.getBlue());
+                    newHeight[y] = 255;
+                }
+                oldMatrix[x] = oldHeight;
+                newMatrix[x] = newHeight;
+            }
+
+            var coefWidth = dilationCoefficients.length;
+            var coefHeight = dilationCoefficients[0].length;
+
+            var coefWidthMin = -coefCenterX;
+            var coefWidthMax = coefWidth - coefCenterX - 1;
+
+            var coefHeightMin = -coefCenterY;
+            var coefHeightMax = coefHeight - coefCenterY - 1;
+
+            for (var x = 0; x < width; x++) {
+                for (var y = 0; y < height; y++) {
+                    if (oldMatrix[x][y] != 0) {
+                        continue;
+                    }
+                    for (var i = coefWidthMin; i <= coefWidthMax; i++) {
+                        for (var j = coefHeightMin; j <= coefHeightMax; j++) {
+                            var x1 = x + i;
+                            var y1 = y + j;
+
+                            if (x1 < 0 || x1 >= width) {
+                                continue;
+                            }
+                            if (y1 < 0 || y1 >= height) {
+                                continue;
+                            }
+                            if (x1 == x && y1 == y) {
+                                continue;
+                            }
+                            if (dilationCoefficients[i + coefCenterX][j + coefCenterY] == 255) {
+                                newMatrix[x1][y1] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+
+            System.out.println(System.currentTimeMillis() - start);
+
+            for (var x = 0; x < width; x++) {
+                for (var y = 0; y < height; y++) {
+                    var color = new Color(
+                        RGB.checkBorderValues(newMatrix[x][y]),
+                        RGB.checkBorderValues(newMatrix[x][y]),
+                        RGB.checkBorderValues(newMatrix[x][y])
+                    );
+
+                    bufferedImage.setRGB(x, y, color.getRGB());
+                }
+            }
+
+            view.refreshFilterPhotosSection(getInputStreamFromBufferedImage(oldBufferedImage), bufferedImage, getInputStreamFromBufferedImage(bufferedImage));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void dilationFast(BufferedImage oldBufferedImage, List<List<String>> dilationCoefficientsValues) {
+        try {
+            var coefCenterX = 0;
+            var coefCenterY = 0;
+            var dilationCoefficients = new int[dilationCoefficientsValues.get(0).size()][dilationCoefficientsValues.size()];
+
+            for (var i = 0; i < dilationCoefficientsValues.get(0).size(); i++) {
+                dilationCoefficients[i] = new int[dilationCoefficientsValues.size()];
+                for (var j = 0; j < dilationCoefficientsValues.size(); j++) {
+                    dilationCoefficients[i][j] = Integer.parseInt(dilationCoefficientsValues.get(i).get(j)) * 255;
+                    if (dilationCoefficients[i][j] == 510) {
+                        coefCenterX = i;
+                        coefCenterY = j;
+                    }
+                }
+            }
+
+            var width = oldBufferedImage.getWidth();
+            var height = oldBufferedImage.getHeight();
+
+            var bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+            var oldMatrix = new int[width][height];
+            var newMatrix = new int[width][height];
+
+            var start = System.currentTimeMillis();
+
+            for (var x = 0; x < width; x++) {
+                var oldHeight = new int[height];
+                var newHeight = new int[height];
+
+                for (var y = 0; y < height; y++) {
+                    var rgb = oldBufferedImage.getRGB(x, y);
+                    var color = new Color(rgb);
+
+                    oldHeight[y] = (int) (0.2125 * color.getRed() + 0.7154 * color.getGreen() + 0.0721 * color.getBlue());
+                    newHeight[y] = 255;
+                }
+                oldMatrix[x] = oldHeight;
+                newMatrix[x] = newHeight;
+            }
+
+            var coefWidth = dilationCoefficients.length;
+            var coefHeight = dilationCoefficients[0].length;
+
+            var coefWidthMin = -coefCenterX;
+            var coefWidthMax = coefWidth - coefCenterX - 1;
+
+            var coefHeightMin = -coefCenterY;
+            var coefHeightMax = coefHeight - coefCenterY - 1;
+
+            for (var x = 0; x < width; x++) {
+                for (var y = 0; y < height; y++) {
+                    if (oldMatrix[x][y] != 0) {
+                        continue;
+                    }
+                    if (oldMatrix[Math.max(0, x - 1)][y] == 0
+                        && oldMatrix[Math.min(width - 1, x + 1)][y] == 0
+                        && oldMatrix[x][Math.max(0, y - 1)] == 0
+                        && oldMatrix[x][Math.min(height - 1, y + 1)] == 0) {
+                        newMatrix[x][y] = 0;
+                        newMatrix[Math.max(0, x - 1)][y] = 0;
+                        newMatrix[Math.min(width - 1, x + 1)][y] = 0;
+                        newMatrix[x][Math.max(0, y - 1)] = 0;
+                        newMatrix[x][Math.min(height - 1, y + 1)] = 0;
+                        continue;
+                    }
+                    for (var i = coefWidthMin; i <= coefWidthMax; i++) {
+                        for (var j = coefHeightMin; j <= coefHeightMax; j++) {
+                            var x1 = x + i;
+                            var y1 = y + j;
+
+                            if (x1 < 0 || x1 >= width) {
+                                continue;
+                            }
+                            if (y1 < 0 || y1 >= height) {
+                                continue;
+                            }
+                            if (x1 == x && y1 == y) {
+                                continue;
+                            }
+                            if (dilationCoefficients[i + coefCenterX][j + coefCenterY] == 255) {
+                                newMatrix[x1][y1] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+
+            System.out.println(System.currentTimeMillis() - start);
+
+            for (var x = 0; x < width; x++) {
+                for (var y = 0; y < height; y++) {
+                    var color = new Color(
+                        RGB.checkBorderValues(newMatrix[x][y]),
+                        RGB.checkBorderValues(newMatrix[x][y]),
+                        RGB.checkBorderValues(newMatrix[x][y])
+                    );
+
+                    bufferedImage.setRGB(x, y, color.getRGB());
+                }
+            }
+
+            view.refreshFilterPhotosSection(getInputStreamFromBufferedImage(oldBufferedImage), bufferedImage, getInputStreamFromBufferedImage(bufferedImage));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void erosion(BufferedImage oldBufferedImage, List<List<String>> dilationCoefficientsValues) {
+        try {
+            var coefCenterX = 0;
+            var coefCenterY = 0;
+            var dilationCoefficients = new int[dilationCoefficientsValues.get(0).size()][dilationCoefficientsValues.size()];
+
+            for (var i = 0; i < dilationCoefficientsValues.get(0).size(); i++) {
+                dilationCoefficients[i] = new int[dilationCoefficientsValues.size()];
+                for (var j = 0; j < dilationCoefficientsValues.size(); j++) {
+                    dilationCoefficients[i][j] = Integer.parseInt(dilationCoefficientsValues.get(i).get(j)) * 255;
+                    if (dilationCoefficients[i][j] == 510) {
+                        coefCenterX = i;
+                        coefCenterY = j;
+                    }
+                }
+            }
+
+            var width = oldBufferedImage.getWidth();
+            var height = oldBufferedImage.getHeight();
+
+            var bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+            var oldMatrix = new int[width][height];
+            var newMatrix = new int[width][height];
+
+            var start = System.currentTimeMillis();
+
+            for (var x = 0; x < width; x++) {
+                var oldHeight = new int[height];
+                var newHeight = new int[height];
+
+                for (var y = 0; y < height; y++) {
+                    var rgb = oldBufferedImage.getRGB(x, y);
+                    var color = new Color(rgb);
+
+                    oldHeight[y] = (int) (0.2125 * color.getRed() + 0.7154 * color.getGreen() + 0.0721 * color.getBlue());
+                    newHeight[y] = 255;
+                }
+                oldMatrix[x] = oldHeight;
+                newMatrix[x] = newHeight;
+            }
+
+            var coefWidth = dilationCoefficients.length;
+            var coefHeight = dilationCoefficients[0].length;
+
+            var coefWidthMin = -coefCenterX;
+            var coefWidthMax = coefWidth - coefCenterX - 1;
+
+            var coefHeightMin = -coefCenterY;
+            var coefHeightMax = coefHeight - coefCenterY - 1;
+
+            for (var x = 0; x < width; x++) {
+                for (var y = 0; y < height; y++) {
+                    if (oldMatrix[x][y] != 0) {
+                        continue;
+                    }
+                    var flag = false;
+                    for (var i = coefWidthMin; i <= coefWidthMax; i++) {
+                        for (var j = coefHeightMin; j <= coefHeightMax; j++) {
+                            var x1 = x + i;
+                            var y1 = y + j;
+
+                            if (x1 < 0 || x1 >= width) {
+                                continue;
+                            }
+                            if (y1 < 0 || y1 >= height) {
+                                continue;
+                            }
+                            if (x1 == x && y1 == y) {
+                                continue;
+                            }
+                            if (dilationCoefficients[i + coefCenterX][j + coefCenterY] == 255 && oldMatrix[x1][y1] == 255) {
+                                flag = true;
+                            }
+                        }
+                    }
+                    newMatrix[x][y] = flag ? 255 : 0;
+                }
+            }
+
+            System.out.println(System.currentTimeMillis() - start);
+
+            for (var x = 0; x < width; x++) {
+                for (var y = 0; y < height; y++) {
+                    var color = new Color(
+                        RGB.checkBorderValues(newMatrix[x][y]),
+                        RGB.checkBorderValues(newMatrix[x][y]),
+                        RGB.checkBorderValues(newMatrix[x][y])
+                    );
+
+                    bufferedImage.setRGB(x, y, color.getRGB());
+                }
+            }
+
+            view.refreshFilterPhotosSection(getInputStreamFromBufferedImage(oldBufferedImage), bufferedImage, getInputStreamFromBufferedImage(bufferedImage));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void erosionFast(BufferedImage oldBufferedImage, List<List<String>> dilationCoefficientsValues) {
+        try {
+            var coefCenterX = 0;
+            var coefCenterY = 0;
+            var dilationCoefficients = new int[dilationCoefficientsValues.get(0).size()][dilationCoefficientsValues.size()];
+
+            for (var i = 0; i < dilationCoefficientsValues.get(0).size(); i++) {
+                dilationCoefficients[i] = new int[dilationCoefficientsValues.size()];
+                for (var j = 0; j < dilationCoefficientsValues.size(); j++) {
+                    dilationCoefficients[i][j] = Integer.parseInt(dilationCoefficientsValues.get(i).get(j)) * 255;
+                    if (dilationCoefficients[i][j] == 510) {
+                        coefCenterX = i;
+                        coefCenterY = j;
+                    }
+                }
+            }
+
+            var width = oldBufferedImage.getWidth();
+            var height = oldBufferedImage.getHeight();
+
+            var bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+            var oldMatrix = new int[width][height];
+            var newMatrix = new int[width][height];
+
+            var start = System.currentTimeMillis();
+
+            for (var x = 0; x < width; x++) {
+                var oldHeight = new int[height];
+                var newHeight = new int[height];
+
+                for (var y = 0; y < height; y++) {
+                    var rgb = oldBufferedImage.getRGB(x, y);
+                    var color = new Color(rgb);
+
+                    oldHeight[y] = (int) (0.2125 * color.getRed() + 0.7154 * color.getGreen() + 0.0721 * color.getBlue());
+                    newHeight[y] = 255;
+                }
+                oldMatrix[x] = oldHeight;
+                newMatrix[x] = newHeight;
+            }
+
+            var coefWidth = dilationCoefficients.length;
+            var coefHeight = dilationCoefficients[0].length;
+
+            var coefWidthMin = -coefCenterX;
+            var coefWidthMax = coefWidth - coefCenterX - 1;
+
+            var coefHeightMin = -coefCenterY;
+            var coefHeightMax = coefHeight - coefCenterY - 1;
+
+            for (var x = 0; x < width; x++) {
+                for (var y = 0; y < height; y++) {
+                    if (oldMatrix[x][y] != 0) {
+                        continue;
+                    }
+                    newMatrix[x][y] = a(coefWidthMin, coefWidthMax, coefHeightMin, coefHeightMax, x, y, dilationCoefficients, oldMatrix, width, height, coefCenterX, coefCenterY) ? 255 : 0;
+                }
+            }
+
+            System.out.println(System.currentTimeMillis() - start);
+
+            for (var x = 0; x < width; x++) {
+                for (var y = 0; y < height; y++) {
+                    var color = new Color(
+                        RGB.checkBorderValues(newMatrix[x][y]),
+                        RGB.checkBorderValues(newMatrix[x][y]),
+                        RGB.checkBorderValues(newMatrix[x][y])
+                    );
+
+                    bufferedImage.setRGB(x, y, color.getRGB());
+                }
+            }
+
+            view.refreshFilterPhotosSection(getInputStreamFromBufferedImage(oldBufferedImage), bufferedImage, getInputStreamFromBufferedImage(bufferedImage));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean a(int coefWidthMin,
+                      int coefWidthMax,
+                      int coefHeightMin,
+                      int coefHeightMax,
+                      int x,
+                      int y,
+                      int[][] dilationCoefficients,
+                      int[][] oldMatrix,
+                      int width,
+                      int height,
+                      int coefCenterX,
+                      int coefCenterY) {
+        for (var i = coefWidthMin; i <= coefWidthMax; i++) {
+            for (var j = coefHeightMin; j <= coefHeightMax; j++) {
+                var x1 = x + i;
+                var y1 = y + j;
+
+                if (x1 < 0 || x1 >= width) {
+                    continue;
+                }
+                if (y1 < 0 || y1 >= height) {
+                    continue;
+                }
+                if (x1 == x && y1 == y) {
+                    continue;
+                }
+                if (dilationCoefficients[i + coefCenterX][j + coefCenterY] == 255 && oldMatrix[x1][y1] == 255) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
--- src/main/java/com/kkoz/parallels/lab_3/binarisation/Lab3BinarisationView.java
+++ src/main/java/com/kkoz/parallels/lab_3/binarisation/Lab3BinarisationView.java
@@ -8,6 +8,7 @@ import com.vaadin.flow.component.HasComponents;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.upload.Upload;
 import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
@@ -15,7 +16,12 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.StreamResource;
 import org.apache.commons.lang3.StringUtils;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 @Route("/labs/3/binarisation")
 public class Lab3BinarisationView extends View<Lab3BinarisationPresenter> {
@@ -23,7 +29,12 @@ public class Lab3BinarisationView extends View<Lab3BinarisationPresenter> {
     private final HorizontalLayout imageSection = new HorizontalLayout();
     private final TextField thresholdTextField = new TextField();
     private final TextField bradleyAreaTextField = new TextField();
+    private final VerticalLayout dilationErosionMatrixSection = new VerticalLayout();
+    private final TextField dilationErosionWidthTextField = new TextField();
+    private final TextField dilationErosionHeightTextField = new TextField();
+    private final List<List<TextField>> dilationErosionCoefficients = new ArrayList<>();
 
+    private BufferedImage filteredBufferedImage;
     private String photoFileName;
 
     public Lab3BinarisationView() {
@@ -32,7 +43,8 @@ public class Lab3BinarisationView extends View<Lab3BinarisationPresenter> {
         add(
             createUploadPhotoSection(),
             imageSection,
-            createBinarisationTypeSection()
+            createBinarisationTypeSection(),
+            createDilationErosionSection()
         );
     }
 
@@ -41,7 +53,11 @@ public class Lab3BinarisationView extends View<Lab3BinarisationPresenter> {
 
         upload.addSucceededListener(event -> {
             photoFileName = event.getFileName();
-            refreshFilterPhotosSection(buffer.getInputStream(photoFileName));
+            try {
+                refreshFilterPhotosSection(ImageIO.read(buffer.getInputStream(photoFileName)), buffer.getInputStream(photoFileName));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             upload.clearFileList();
         });
 
@@ -81,6 +97,136 @@ public class Lab3BinarisationView extends View<Lab3BinarisationPresenter> {
         return result;
     }
 
+    private Component createDilationErosionSection() {
+        dilationErosionWidthTextField.setLabel("Ширина");
+        dilationErosionWidthTextField.setValue("3");
+        dilationErosionWidthTextField.addValueChangeListener(e -> {
+            if (e.isFromClient()) {
+                updateDilationErosionMatrixSection(false);
+            }
+        });
+
+        dilationErosionHeightTextField.setLabel("Высота");
+        dilationErosionHeightTextField.setValue("3");
+        dilationErosionHeightTextField.addValueChangeListener(e -> {
+            if (e.isFromClient()) {
+                updateDilationErosionMatrixSection(false);
+            }
+        });
+
+        dilationErosionCoefficients.addAll(
+            List.of(
+                List.of(
+                    new TextField("", "0", ""),
+                    new TextField("", "1", ""),
+                    new TextField("", "0", "")
+                ),
+                List.of(
+                    new TextField("", "1", ""),
+                    new TextField("", "2", ""),
+                    new TextField("", "1", "")
+                ),
+                List.of(
+                    new TextField("", "0", ""),
+                    new TextField("", "1", ""),
+                    new TextField("", "0", "")
+                )
+            )
+        );
+
+        for (var dilationErosionRow : dilationErosionCoefficients) {
+            var matrixRow = new HorizontalLayout();
+            for (var dilationErosionCell : dilationErosionRow) {
+                matrixRow.add(dilationErosionCell);
+            }
+            dilationErosionMatrixSection.add(matrixRow);
+        }
+
+        return new HorizontalLayout(
+            dilationErosionWidthTextField,
+            dilationErosionHeightTextField,
+            dilationErosionMatrixSection,
+            new Button(
+                "Расширить",
+                e -> presenter.dilation(
+                    filteredBufferedImage,
+                    dilationErosionCoefficients.stream().map(row -> row.stream().map(TextField::getValue).toList()).toList()
+                )
+            ),
+            new Button(
+                "Расширить быстро",
+                e -> presenter.dilationFast(
+                    filteredBufferedImage,
+                    dilationErosionCoefficients.stream().map(row -> row.stream().map(TextField::getValue).toList()).toList()
+                )
+            ),
+            new Button(
+                "Сузить",
+                e -> presenter.erosion(
+                    filteredBufferedImage,
+                    dilationErosionCoefficients.stream().map(row -> row.stream().map(TextField::getValue).toList()).toList()
+                )
+            ),
+            new Button(
+                "Сузить быстро",
+                e -> presenter.erosionFast(
+                    filteredBufferedImage,
+                    dilationErosionCoefficients.stream().map(row -> row.stream().map(TextField::getValue).toList()).toList()
+                )
+            ),
+            new Button(
+                "Базовая маска",
+                e -> {
+                    dilationErosionCoefficients.clear();
+                    dilationErosionCoefficients.addAll(
+                        List.of(
+                            List.of(
+                                new TextField("", "0", ""),
+                                new TextField("", "1", ""),
+                                new TextField("", "0", "")
+                            ),
+                            List.of(
+                                new TextField("", "1", ""),
+                                new TextField("", "2", ""),
+                                new TextField("", "1", "")
+                            ),
+                            List.of(
+                                new TextField("", "0", ""),
+                                new TextField("", "1", ""),
+                                new TextField("", "0", "")
+                            )
+                        )
+                    );
+                    updateDilationErosionMatrixSection(true);
+                }
+            )
+        );
+    }
+
+    private void updateDilationErosionMatrixSection(boolean preset) {
+        if (preset) {
+            dilationErosionWidthTextField.setValue("3");
+            dilationErosionHeightTextField.setValue("3");
+        } else {
+            dilationErosionCoefficients.clear();
+            for (var i = 0; i < Integer.parseInt(dilationErosionHeightTextField.getValue()); i++) {
+                dilationErosionCoefficients.add(new ArrayList<>());
+                for (var j = 0; j < Integer.parseInt(dilationErosionWidthTextField.getValue()); j++) {
+                    dilationErosionCoefficients.get(i).add(new TextField("", "0", ""));
+                }
+            }
+        }
+
+        dilationErosionMatrixSection.removeAll();
+        for (var dilationErosionRow : dilationErosionCoefficients) {
+            var matrixRow = new HorizontalLayout();
+            for (var dilationErosionCell : dilationErosionRow) {
+                matrixRow.add(dilationErosionCell);
+            }
+            dilationErosionMatrixSection.add(matrixRow);
+        }
+    }
+
     private void addPhotoToSection(HasComponents section, InputStream imageStream, String name) {
         var image = new Image(
             new StreamResource(name, () -> imageStream),
@@ -90,9 +236,17 @@ public class Lab3BinarisationView extends View<Lab3BinarisationPresenter> {
         section.add(image);
     }
 
-    public void refreshFilterPhotosSection(InputStream imageStream) {
+    public void refreshFilterPhotosSection(BufferedImage filteredBufferedImage, InputStream imageStream) {
+        this.filteredBufferedImage = filteredBufferedImage;
         imageSection.removeAll();
         addPhotoToSection(imageSection, buffer.getInputStream(photoFileName), "До фильтра");
+        addPhotoToSection(imageSection, imageStream, "После фильтра");
+    }
+
+    public void refreshFilterPhotosSection(InputStream sourceImageStream, BufferedImage filteredBufferedImage, InputStream imageStream) {
+        this.filteredBufferedImage = filteredBufferedImage;
+        imageSection.removeAll();
+        addPhotoToSection(imageSection, sourceImageStream, "До фильтра");
         addPhotoToSection(imageSection, imageStream, "После фильтра");
     }
 }
